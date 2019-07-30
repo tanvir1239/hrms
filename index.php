2f675b23fdbebfb8c90d9aac2babcc2a48450068
tanvir1239 <53406654+tanvir1239@users.noreply.github.com>
2019-07-30 20:36:14 +0600

    Some changes were made

--- index.php
+++ index.php
@@ -6,15 +6,6 @@
     });
 </script>
 <?php
-
-// Program to display URL of current page. 
-  
-$link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 
-                "https" : "http") . "://" . $_SERVER['HTTP_HOST'] .  
-                $_SERVER['REQUEST_URI']; 
-  
-echo $link; 
-
 include("db.php");
 session_start();
 //Getting Input value
